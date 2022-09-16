01c472f4af3c7c517ad32ba9de74cee62b2072f7
Guillermo Granados Gómez <pytonicus@gmail.com>
2022-09-16 09:21:54 +0200

    Phase 1 complete

---
+++ src/Form/TaskType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+// cargamos el input submit:
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {   // quitamos los campos que no vamos a usar del formulario:
+        $builder
+            ->add('name')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
